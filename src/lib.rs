e1b6e9a487fff0357027e70243790c88cdf5a1fb
Richard Zak <richard.j.zak@gmail.com>
2025-02-19 12:19:02 -0500

    fix: be more idiomatic, use `Option` instead of -1
    * https://old.reddit.com/r/rust/comments/1irxcp7/new_crate_for_getting_apps_memory_usage/mddqgnw/
    
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,45 +3,47 @@
 #![doc = include_str!("../readme.md")]
 #![deny(missing_docs)]
 
+use std::num::NonZeroU64;
+
 /// Library version
 pub const APP_MEMORY_USAGE_FETCHER_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 extern "C" {
-    fn getMemoryUsage() -> i64;
+    fn getMemoryUsage() -> Option<NonZeroU64>;
 }
 
 /// Application memory usage in bytes
 /// ```
-///  assert!(app_memory_usage_fetcher::get_memory_usage_bytes() > 0);
+///  assert!(app_memory_usage_fetcher::get_memory_usage_bytes().is_some());
 /// ```
 #[inline]
-pub fn get_memory_usage_bytes() -> i64 {
+pub fn get_memory_usage_bytes() -> Option<NonZeroU64> {
     unsafe { getMemoryUsage() }
 }
 
 /// Application memory usage in kilobytes
 #[inline]
-pub fn get_memory_usage_kbytes() -> f64 {
-    unsafe { getMemoryUsage() as f64 / 1024.0f64 }
+pub fn get_memory_usage_kbytes() -> Option<f64> {
+    unsafe { getMemoryUsage().map(|m| m.get() as f64 / 1024.0f64) }
 }
 
 /// Application memory usage in megabytes
 #[inline]
-pub fn get_memory_usage_mbytes() -> f64 {
+pub fn get_memory_usage_mbytes() -> Option<f64> {
     const MEGABYTE: f64 = 1024.0f64 * 1024.0f64;
-    unsafe { getMemoryUsage() as f64 / MEGABYTE }
+    unsafe { getMemoryUsage().map(|m| m.get() as f64 / MEGABYTE) }
 }
 
 /// Application memory usage in gigabytes
 #[inline]
-pub fn get_memory_usage_gbytes() -> f64 {
+pub fn get_memory_usage_gbytes() -> Option<f64> {
     const GIGABYTE: f64 = 1024.0f64 * 1024.0f64 * 1024.0f64;
-    unsafe { getMemoryUsage() as f64 / GIGABYTE }
+    unsafe { getMemoryUsage().map(|m| m.get() as f64 / GIGABYTE) }
 }
 
 /// Application memory usage in terabytes
 #[inline]
-pub fn get_memory_usage_tbytes() -> f64 {
+pub fn get_memory_usage_tbytes() -> Option<f64> {
     const TERABYTE: f64 = 1024.0f64 * 1024.0f64 * 1024.0f64 * 1024.0f64;
-    unsafe { getMemoryUsage() as f64 / TERABYTE }
+    unsafe { getMemoryUsage().map(|m| m.get() as f64 / TERABYTE) }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,25 @@
 use std::process::ExitCode;
 
 fn main() -> Result<ExitCode, ExitCode> {
-    if app_memory_usage_fetcher::get_memory_usage_bytes() < 0 {
+    if app_memory_usage_fetcher::get_memory_usage_bytes().is_none() {
         eprintln!("Operating system {} not supported.", std::env::consts::OS);
         Err(ExitCode::FAILURE)
     } else {
         println!(
             "Memory usage: {} bytes.",
-            app_memory_usage_fetcher::get_memory_usage_bytes()
+            app_memory_usage_fetcher::get_memory_usage_bytes().unwrap()
         );
         println!(
             "Memory usage: {:.2} kilobytes.",
-            app_memory_usage_fetcher::get_memory_usage_kbytes()
+            app_memory_usage_fetcher::get_memory_usage_kbytes().unwrap()
         );
         println!(
             "Memory usage: {:.2} megabytes.",
-            app_memory_usage_fetcher::get_memory_usage_mbytes()
+            app_memory_usage_fetcher::get_memory_usage_mbytes().unwrap()
         );
         println!(
             "Memory usage: {:.2} gigabytes.",
-            app_memory_usage_fetcher::get_memory_usage_gbytes()
+            app_memory_usage_fetcher::get_memory_usage_gbytes().unwrap()
         );
         Ok(ExitCode::SUCCESS)
     }
