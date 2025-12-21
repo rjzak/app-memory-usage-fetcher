aa138e4d4197913b33e799e9b2aa0ef4f4f0c762
Richard Zak <richard.j.zak@gmail.com>
2025-12-21 00:41:23 -0500

    chore: ensure recompile if .c file changes
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 fn main() {
+    println!("cargo:rerun-if-changed=src/os_function_calls.c");
     cc::Build::new()
         .file("src/os_function_calls.c")
         .compile("osfuncs");
